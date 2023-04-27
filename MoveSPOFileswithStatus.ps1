ffaecbd770e75e9bfaf261fa695281bbcc3b32b9
Lewis Livingstone <lewis.livingstone@microbyte.com>
2023-04-27 14:58:57 +0100

    New tool for Moving Items in SharePoint

---
+++ MoveSPOFileswithStatus.ps1
@@ -0,0 +1,29 @@
+$client = "THE ROC"
+
+$listitems
+
+try{
+	$job = Move-PnPFile -SourceUrl "/sites/migration/Clients/Existing/$($client)" -TargetUrl "/sites/company/Clients%20 Existing" -NoWait -Force
+}
+catch{
+	Write-Host "An error occurred:" -ForegroundColor Red
+	Write-Host $_.Exception.Message
+}
+$d1 = Get-Date
+$jobStatus = Receive-PnPCopyMoveJobStatus -Job $job
+while($jobStatus.JobState -eq 4){
+	Write-Host "Getting job status for $($client)..." -ForegroundColor Yellow
+	$progress = ($jobstatus.Logs | ConvertFrom-Json) | ? {$_.Event -like "JobProgress"} | Select -Last 1
+	Write-Host "Processed $($progress.ObjectsProcessed) out of $($progress.TotalExpectedSPObjects)"
+	if ($progress.TotalErrors -gt 0){Write-Host "Current Errors: $($progress.TotalErrors)" -ForegroundColor Red}
+	if($progress.ObjectsProcessed -gt 0 -and $progress.TotalExpectedSPObjects -gt 0){
+		$percent = [math]::Round((($progress.ObjectsProcessed / $progress.TotalExpectedSPObjects) * 100),2)
+		Write-Host "$($percent)% completed"
+	}
+	$d2 = Get-Date
+	$diff = $d2 - $d1
+	Write-Host "Time Elapsed [hh:mm:ss:ms]:  $diff"
+	Start-Sleep 5
+	$jobStatus = Receive-PnPCopyMoveJobStatus -Job $job
+}
+
